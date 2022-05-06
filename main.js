df717f823a5bff571b0eecae0fc28e17653bd541
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-05-06 01:15:55 -0300

    Aula 4/5 concluída

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ function onScroll() {
     navigation.classList.remove("scroll");
   }
 }
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`#home, 
+           #home img, 
+           #home .stats,
+           #services,
+           #services header,
+           #services .card,
+           #about,
+           #about header,
+           #about .content,
+           
+           `);
