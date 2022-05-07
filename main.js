902e482fc8ae57f9550baea9f3e2ea7c1ab1c5aa
João Victor <95556375+HomemJaguar111@users.noreply.github.com>
2022-05-07 05:17:46 -0300

    Faltam duas horas de aula

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add("scroll");
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove("scroll");
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
 function openMenu() {
-  document.body.classList.add("menu-expanded");
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove("menu-expanded");
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
-  origin: "top",
-  distance: "30px",
-  duration: 700,
+  origin: 'top',
+  distance: '30px',
+  duration: 700
 }).reveal(`#home, 
            #home img, 
            #home .stats,
@@ -28,4 +45,4 @@ ScrollReveal({
            #about header,
            #about .content,
            
-           `);
+           `)
